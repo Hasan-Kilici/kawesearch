5421a320a6ff2f7d6415cb90b8eaecf25e6b51db
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2025-01-23 23:25:10 +0300

    Add files via upload

---
+++ examples/svelte/src/routes/basic_search/+page.svelte
@@ -0,0 +1,41 @@
+
+<input type="text" id="search" bind:this={input} on:input={searchPlace} placeholder="Search for a place">
+{#if result}
+    <ul>
+        {#each result as place}
+            <li>{place.name}</li>
+        {/each}
+    </ul>
+{/if}
+<script>
+    import PlaceData from '$lib/data/places.json';
+    import Search from 'kawesearch';
+
+    let input;
+
+    const synonyms = {
+        otel: ['otel', 'konaklama', 'tatil', 'lüks'],
+        restoran: ['restoran', 'yemek', 'cafe', 'kahvaltı'],
+        gezilecek: ['gezilecek', 'mekan', 'doğa', 'tarih'],
+    };
+
+    let search = new Search(PlaceData, synonyms,{
+        language: "tr", 
+        algorithm: "jaro-winkler",
+        threshold: 0.4,
+        debounceDelay: 300,
+        customMessages: {
+            tr:{
+                noMatch: "Sonuc bulunamadı my mannn",
+                suggestion: "Bunu mu diyon brother?",
+            }
+        }
+    })
+
+    let result = [];
+
+    async function searchPlace() {
+        result = await search.search(input.value);
+    }
+
+</script>
---
+++ examples/svelte/src/routes/suggestion_search/+page.svelte
@@ -0,0 +1,45 @@
+
+<input type="text" id="search" bind:this={input} on:input={searchPlace} placeholder="Search for a place">
+{#if result}
+    {#if result.suggestions && result.suggestions.length > 0}
+        <p>{result.message} {result.suggestions[0].name}</p>
+        <ul>
+            {#each result.suggestions as place}
+                <li>{place.name}</li>
+            {/each}
+        </ul>
+    {:else}
+        {#each result as place}
+                <li>{place.name}</li>
+        {/each}
+    {/if}
+{/if}
+<script>
+    import PlaceData from '$lib/data/places.json';
+    import Search from 'kawesearch';
+
+    let input;
+
+    const synonyms = {
+        otel: ['otel', 'konaklama', 'tatil', 'lüks'],
+        restoran: ['restoran', 'yemek', 'cafe', 'kahvaltı'],
+        gezilecek: ['gezilecek', 'mekan', 'doğa', 'tarih'],
+    };
+
+    let search = new Search(PlaceData, synonyms,{
+        language: "en", 
+        algorithm: "levenshtein",
+        threshold: 0.7,
+        debounceDelay: 300,
+        suggestOnNoMatch: true,
+        suggestionThreshold: 0.4,
+    })
+
+    let result = [];
+
+    async function searchPlace() {
+        result = await search.search(input.value);
+        console.log(result);
+    }
+
+</script>
